88b9a7706642383ca59f60b7e33d0effce784145
farisizia <izziasroris@gmail.com>
2025-06-24 17:05:26 +0700

    check

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ function Hero() {
   return (
     <section id="hero" style={{ padding: "4rem", textAlign: "center" }}>
       <h1>
-        Hi 👋, I’m <span style={{ color: "#00BFFF" }}>Faris Izia</span>
+        Hi 👋, I’m <span style={{ color: "#00BFFF" }}>Faris Izzi Asrori</span>
       </h1>
       <p>Frontend Developer • QA Engineer • Tech Explorer</p>
       <a href="#projects">
