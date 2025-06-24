816c438d6d5b8a3f3fb8024350710d8ba9b4b0a7
farisizia <izziasroris@gmail.com>
2025-06-24 16:48:43 +0700

    fix bug

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    env: {
+      BASE_URL: process.env.BASE_URL,
     },
   },
 });
