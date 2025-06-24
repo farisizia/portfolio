c44d30f85276cd68dfc29594976252d956d51099
farisizia <izziasroris@gmail.com>
2025-06-24 16:43:15 +0700

    perbaiki cy.visit

--- cypress/e2e/portfolio.cy.js
+++ cypress/e2e/portfolio.cy.js
@@ -1,6 +1,6 @@
 describe("Tes Portfolio", () => {
   it("Buka halaman utama", () => {
-    cy.visit("http://localhost:5173");
+    cy.visit("http://localhost:4173");
     cy.contains("Halo, Faris").should("be.visible"); // sesuaikan dengan isi halaman kamu
   });
 });
